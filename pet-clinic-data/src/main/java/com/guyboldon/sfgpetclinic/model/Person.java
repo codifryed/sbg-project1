60939e0d432e8046a888d9c0be189cd2b33f93d7
Guy Boldon <guy@programmingtolife.com>
2019-01-05 21:14:18 +0100

    Add base Entity and model object IDs. closes #10

--- pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.guyboldon.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
 
--- pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.guyboldon.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private Owner owner;
     private LocalDate birthdate;
 
