6cc416eb59a492595b62001db9f9b0376d306116
Guy Boldon <gvboldon@gmail.com>
2019-01-13 13:31:02 +0100

    Adding Index Page and Controller. closes #8

---
+++ pet-clinic-web/src/main/java/com/guyboldon/sfgpetclinic/Controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.guyboldon.sfgpetclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
