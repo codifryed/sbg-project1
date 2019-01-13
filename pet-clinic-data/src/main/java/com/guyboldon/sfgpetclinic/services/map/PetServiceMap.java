c900681ba9f71c67c432d7e1e5d3203898233871
Guy Boldon <gvboldon@gmail.com>
2019-01-13 13:03:45 +0100

    implement serviceMaps. closes #6

---
+++ pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.guyboldon.sfgpetclinic.services.map;
+
+import com.guyboldon.sfgpetclinic.model.Pet;
+import com.guyboldon.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
