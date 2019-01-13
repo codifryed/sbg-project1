1169ab7e36e4dca3a797b5be2c59b957854d114c
Guy Boldon <gvboldon@gmail.com>
2019-01-13 12:28:05 +0100

    refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/guyboldon/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.guyboldon.sfgpetclinic.services;
 
 import com.guyboldon.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findbyId(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
